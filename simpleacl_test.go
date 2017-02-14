6d06f6e276c529f0120c7edfa3c70ac932b3364f
Jar Dmitrii <jar.dmitrii@gmail.com>
2017-02-14 16:16:37 +0200

    Add test case when all is denied except one action for one endpoint and one user.

--- simpleacl_test.go
+++ simpleacl_test.go
@@ -15,42 +15,91 @@ type testCase struct {
 	rules, check []rule
 }
 
-func TestHasRight (t *testing.T) {
-	testCases := []testCase{
-		{
-			rules: []rule{
-				{simpleacl.ANY, simpleacl.ANY, simpleacl.ANY, simpleacl.ALLOW},
-			},
-			check: []rule{
-				{simpleacl.ANY, simpleacl.ANY, simpleacl.ANY, simpleacl.ALLOW},
+var (
+	allAllowedCase testCase = testCase{
+		rules: []rule{
+			{simpleacl.ANY, simpleacl.ANY, simpleacl.ANY, simpleacl.ALLOW},
+		},
+		check: []rule{
+			{simpleacl.ANY, simpleacl.ANY, simpleacl.ANY, simpleacl.ALLOW},
 
-				{simpleacl.ANY, simpleacl.ANY, simpleacl.CREATE, simpleacl.ALLOW},
-				{simpleacl.ANY, simpleacl.ANY, simpleacl.READ, simpleacl.ALLOW},
-				{simpleacl.ANY, simpleacl.ANY, simpleacl.UPDATE, simpleacl.ALLOW},
-				{simpleacl.ANY, simpleacl.ANY, simpleacl.DELETE, simpleacl.ALLOW},
+			{simpleacl.ANY, simpleacl.ANY, simpleacl.CREATE, simpleacl.ALLOW},
+			{simpleacl.ANY, simpleacl.ANY, simpleacl.READ, simpleacl.ALLOW},
+			{simpleacl.ANY, simpleacl.ANY, simpleacl.UPDATE, simpleacl.ALLOW},
+			{simpleacl.ANY, simpleacl.ANY, simpleacl.DELETE, simpleacl.ALLOW},
 
-				{simpleacl.ANY, "endpoint1", simpleacl.ANY, simpleacl.ALLOW},
-				{"user1", simpleacl.ANY, simpleacl.ANY, simpleacl.ALLOW},
+			{simpleacl.ANY, "endpoint1", simpleacl.ANY, simpleacl.ALLOW},
+			{"user1", simpleacl.ANY, simpleacl.ANY, simpleacl.ALLOW},
 
-				{simpleacl.ANY, "endpoint1", simpleacl.CREATE, simpleacl.ALLOW},
-				{simpleacl.ANY, "endpoint1", simpleacl.READ, simpleacl.ALLOW},
-				{simpleacl.ANY, "endpoint1", simpleacl.UPDATE, simpleacl.ALLOW},
-				{simpleacl.ANY, "endpoint1", simpleacl.DELETE, simpleacl.ALLOW},
+			{simpleacl.ANY, "endpoint1", simpleacl.CREATE, simpleacl.ALLOW},
+			{simpleacl.ANY, "endpoint1", simpleacl.READ, simpleacl.ALLOW},
+			{simpleacl.ANY, "endpoint1", simpleacl.UPDATE, simpleacl.ALLOW},
+			{simpleacl.ANY, "endpoint1", simpleacl.DELETE, simpleacl.ALLOW},
 
-				{"user1", simpleacl.ANY, simpleacl.CREATE, simpleacl.ALLOW},
-				{"user1", simpleacl.ANY, simpleacl.READ, simpleacl.ALLOW},
-				{"user1", simpleacl.ANY, simpleacl.UPDATE, simpleacl.ALLOW},
-				{"user1", simpleacl.ANY, simpleacl.DELETE, simpleacl.ALLOW},
+			{"user1", simpleacl.ANY, simpleacl.CREATE, simpleacl.ALLOW},
+			{"user1", simpleacl.ANY, simpleacl.READ, simpleacl.ALLOW},
+			{"user1", simpleacl.ANY, simpleacl.UPDATE, simpleacl.ALLOW},
+			{"user1", simpleacl.ANY, simpleacl.DELETE, simpleacl.ALLOW},
 
-				{"user1", "endpoint1", simpleacl.ANY, simpleacl.ALLOW},
+			{"user1", "endpoint1", simpleacl.ANY, simpleacl.ALLOW},
 
-				{"user1", "endpoint1", simpleacl.CREATE, simpleacl.ALLOW},
-				{"user1", "endpoint1", simpleacl.READ, simpleacl.ALLOW},
-				{"user1", "endpoint1", simpleacl.UPDATE, simpleacl.ALLOW},
-				{"user1", "endpoint1", simpleacl.DELETE, simpleacl.ALLOW},
-			},
+			{"user1", "endpoint1", simpleacl.CREATE, simpleacl.ALLOW},
+			{"user1", "endpoint1", simpleacl.READ, simpleacl.ALLOW},
+			{"user1", "endpoint1", simpleacl.UPDATE, simpleacl.ALLOW},
+			{"user1", "endpoint1", simpleacl.DELETE, simpleacl.ALLOW},
 		},
 	}
+	allDeniedOneAllowedCase testCase = testCase{
+		rules: []rule{
+			{simpleacl.ANY, simpleacl.ANY, simpleacl.ANY, simpleacl.DENY},
+			{"user2", "endpoint1", simpleacl.CREATE, simpleacl.ALLOW},
+		},
+		check: []rule{
+			{simpleacl.ANY, simpleacl.ANY, simpleacl.ANY, simpleacl.DENY},
+
+			{simpleacl.ANY, simpleacl.ANY, simpleacl.CREATE, simpleacl.DENY},
+			{simpleacl.ANY, simpleacl.ANY, simpleacl.READ, simpleacl.DENY},
+			{simpleacl.ANY, simpleacl.ANY, simpleacl.UPDATE, simpleacl.DENY},
+			{simpleacl.ANY, simpleacl.ANY, simpleacl.DELETE, simpleacl.DENY},
+
+			{simpleacl.ANY, "endpoint1", simpleacl.ANY, simpleacl.DENY},
+			{"user1", simpleacl.ANY, simpleacl.ANY, simpleacl.DENY},
+
+			{simpleacl.ANY, "endpoint1", simpleacl.CREATE, simpleacl.DENY},
+			{simpleacl.ANY, "endpoint1", simpleacl.READ, simpleacl.DENY},
+			{simpleacl.ANY, "endpoint1", simpleacl.UPDATE, simpleacl.DENY},
+			{simpleacl.ANY, "endpoint1", simpleacl.DELETE, simpleacl.DENY},
+
+			{"user1", simpleacl.ANY, simpleacl.CREATE, simpleacl.DENY},
+			{"user1", simpleacl.ANY, simpleacl.READ, simpleacl.DENY},
+			{"user1", simpleacl.ANY, simpleacl.UPDATE, simpleacl.DENY},
+			{"user1", simpleacl.ANY, simpleacl.DELETE, simpleacl.DENY},
+
+			{"user1", "endpoint1", simpleacl.ANY, simpleacl.DENY},
+
+			{"user1", "endpoint1", simpleacl.CREATE, simpleacl.DENY},
+			{"user1", "endpoint1", simpleacl.READ, simpleacl.DENY},
+			{"user1", "endpoint1", simpleacl.UPDATE, simpleacl.DENY},
+			{"user1", "endpoint1", simpleacl.DELETE, simpleacl.DENY},
+
+			{"user2", "endpoint1", simpleacl.CREATE, simpleacl.ALLOW},
+			{"user2", "endpoint1", simpleacl.READ, simpleacl.DENY},
+			{"user2", "endpoint1", simpleacl.UPDATE, simpleacl.DENY},
+			{"user2", "endpoint1", simpleacl.DELETE, simpleacl.DENY},
+
+			{"user2", simpleacl.ANY, simpleacl.CREATE, simpleacl.DENY},
+			{"user2", simpleacl.ANY, simpleacl.READ, simpleacl.DENY},
+			{"user2", simpleacl.ANY, simpleacl.UPDATE, simpleacl.DENY},
+			{"user2", simpleacl.ANY, simpleacl.DELETE, simpleacl.DENY},
+
+			{"user2", "endpoint1", simpleacl.ANY, simpleacl.DENY},
+
+		},
+	}
+)
+
+func TestHasRight (t *testing.T) {
+	testCases := []testCase{allAllowedCase, allDeniedOneAllowedCase}
 
 	acl := simpleacl.Acl
 	for _, testCase := range testCases {
