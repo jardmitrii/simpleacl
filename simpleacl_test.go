e1d2351782cb8743acf8e84da83d740c0b39df62
Jar Dmitrii <jar.dmitrii@gmail.com>
2017-02-14 15:31:47 +0200

    Improve test of basic rule

--- simpleacl_test.go
+++ simpleacl_test.go
@@ -6,42 +6,69 @@ import (
 	"testing"
 )
 
+type rule struct {
+	user, endpoint, action string
+	allow bool
+}
+
+type testCase struct {
+	rules, check []rule
+}
+
 func TestHasRight (t *testing.T) {
-	test_rules := [][]string{
-		{"user1", "endpoint1", simpleacl.CREATE,},
-		{"user1", "endpoint1", simpleacl.READ,},
-		{"user1", "endpoint1", simpleacl.UPDATE,},
-		{"user1", "endpoint1", simpleacl.DELETE,},
-		{"user2", "endpoint2", simpleacl.CREATE,},
-		{"user2", "endpoint2", simpleacl.READ,},
-		{"user2", "endpoint2", simpleacl.UPDATE,},
-		{"user2", "endpoint2", simpleacl.DELETE,},
-		{"user3", "", simpleacl.CREATE,},
-		{"user3", "", simpleacl.READ,},
-		{"user3", "", simpleacl.UPDATE,},
-		{"user3", "", simpleacl.DELETE,},
-		{"", "endpoint4", simpleacl.CREATE,},
-		{"", "endpoint4", simpleacl.READ,},
-		{"", "endpoint4", simpleacl.UPDATE,},
-		{"", "endpoint4", simpleacl.DELETE,},
-		{"user5", "endpoint5", "",},
-		{"user5", "endpoint5", "",},
-		{"user5", "endpoint5", "",},
-		{"user5", "endpoint5", "",},
+	testCases := []testCase{
+		{
+			rules: []rule{
+				{simpleacl.ANY, simpleacl.ANY, simpleacl.ANY, simpleacl.ALLOW},
+			},
+			check: []rule{
+				{simpleacl.ANY, simpleacl.ANY, simpleacl.ANY, simpleacl.ALLOW},
+
+				{simpleacl.ANY, simpleacl.ANY, simpleacl.CREATE, simpleacl.ALLOW},
+				{simpleacl.ANY, simpleacl.ANY, simpleacl.READ, simpleacl.ALLOW},
+				{simpleacl.ANY, simpleacl.ANY, simpleacl.UPDATE, simpleacl.ALLOW},
+				{simpleacl.ANY, simpleacl.ANY, simpleacl.DELETE, simpleacl.ALLOW},
+
+				{simpleacl.ANY, "endpoint1", simpleacl.ANY, simpleacl.ALLOW},
+				{"user1", simpleacl.ANY, simpleacl.ANY, simpleacl.ALLOW},
+
+				{simpleacl.ANY, "endpoint1", simpleacl.CREATE, simpleacl.ALLOW},
+				{simpleacl.ANY, "endpoint1", simpleacl.READ, simpleacl.ALLOW},
+				{simpleacl.ANY, "endpoint1", simpleacl.UPDATE, simpleacl.ALLOW},
+				{simpleacl.ANY, "endpoint1", simpleacl.DELETE, simpleacl.ALLOW},
+
+				{"user1", simpleacl.ANY, simpleacl.CREATE, simpleacl.ALLOW},
+				{"user1", simpleacl.ANY, simpleacl.READ, simpleacl.ALLOW},
+				{"user1", simpleacl.ANY, simpleacl.UPDATE, simpleacl.ALLOW},
+				{"user1", simpleacl.ANY, simpleacl.DELETE, simpleacl.ALLOW},
+
+				{"user1", "endpoint1", simpleacl.ANY, simpleacl.ALLOW},
+
+				{"user1", "endpoint1", simpleacl.CREATE, simpleacl.ALLOW},
+				{"user1", "endpoint1", simpleacl.READ, simpleacl.ALLOW},
+				{"user1", "endpoint1", simpleacl.UPDATE, simpleacl.ALLOW},
+				{"user1", "endpoint1", simpleacl.DELETE, simpleacl.ALLOW},
+			},
+		},
 	}
 
 	acl := simpleacl.Acl
-	for _, allow := range []bool{simpleacl.ALLOW, simpleacl.DENY} {
-		for _, v := range test_rules {
-			acl.AddRule(v[0], v[1], v[2], allow)
-			has_right := acl.HasRight(v[0], v[1], v[2])
-			if has_right != allow {
-				t.Fatal("TestHasRight", v[0])
+	for _, testCase := range testCases {
+		for _, rule := range testCase.rules {
+			acl.AddRule(rule.user, rule.endpoint, rule.action, rule.allow)
+		}
+
+		for _, check := range testCase.check {
+			has_right := acl.HasRight(check.user, check.endpoint, check.action)
+			if has_right != check.allow {
+				t.Log("Rules: ", acl)
+				t.Log("Check: ", check)
+				t.Fatal("Expect", check.allow)
 			}
 		}
+
+		for _, rule := range testCase.rules {
+			acl.DeleteRule(rule.user, rule.endpoint, rule.action)
+		}
 	}
-}
-
-func TestDeleteRule (t *testing.T) {
-
 }
